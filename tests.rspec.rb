deaea9c12ef155f51723835cd136cda02359ba29
mkwardakov <kwardakov@gmail.com>
2016-02-06 17:25:33 +0300

    exec file is finished

--- tests.rspec.rb
+++ tests.rspec.rb
@@ -1,79 +1,93 @@
 #!/home/user/.rvm/rubies/ruby-2.2.1/bin/ruby
 #/usr/local/bin/rspec
-#require 'rubygems'
 require 'json'
 require 'rspec'
 require 'rest-client'
 require 'faker'
 require 'mongo'
-#require 'pp'
 
 json = File.read('scenario.json')
-testData = JSON.parse(json)
-dbParams = testData["database"]
-uri = testData["uri"]
+$testData = JSON.parse(json)
 
-def apiPost(data)
+$uri = $testData["uri"]
+
+Mongo::Logger.logger.level = ::Logger::WARN #Stop Mongo being noisy
+$dbConnect = Mongo::Client.new($testData["database"])
+
+def apiPost(url, data)
 	begin
-		reply = RestClient::Request.execute(
-			:method => :post,
-			:url => uri,
-			:payload => data.to_json, 
-			:headers => {
-				:accept => :json,
-				"X-Test-track" => "Payment GW test"
-			}
-		)
+		reply = RestClient.post url, data.to_json, :content_type => :json
 	rescue => err
 		reply = err.response
 	end
 	return reply
 end
 
-def make_data(test, card)
+def preparePayment(test)
 	# Form request data. Currency, amount and expected reply code is from test description, card data from the scenario
 	data = Hash.new
-	data["amount"] = test["amount"].to_s
+	data["amount"] = test["amount"]
 	data["currency"] = test["currency"]
 	data["fullName"] = Faker::Name.name
-	data["firstName"] = card["first name"] == "" ? Faker::Name.first_name : card["first name"]
-	data["lastName"] = card["last name"] == "" ? Faker::Name.last_name : card["last name"]
+
+	# If card for test is string, take predefined card which is referred by. Otherwise, use card specified in test
+	card = (test["card"].instance_of? String) ? card = $testData[test["card"]].clone : test["card"].clone
+
+	data["firstName"] = (card["first name"] == "") ? Faker::Name.first_name : card["first name"]
+	data["lastName"] = (card["last name"] == "") ? Faker::Name.last_name : card["last name"]
 	data["cardNumber"] = card["number"]
-	data["cardCVV"] = card["cvv"] == "" ? rand(10).to_s + rand(10).to_s + rand(10).to_s : card["cvv"]
+	data["cardCVV"] = (card["cvv"] == "") ? (rand(10)*100 + rand(10)*10 + rand(10)) : card["cvv"]
 	data["expireMonth"] = card["expM"]
 	data["expireYear"] = card["expY"]
 	return data
 end
 
-#{"amount":"1","currency":"USD","fullName":"john m. connor","firstName":"john","lastName":"connor","cardNumber":"377127687066544","cardCVV":"123","expireMonth":"3","expireYear":"2021"}
-#201
-#500 Response Status : 400
-#500 AMEX is possible to use only for USD
-
-puts uri
-puts dbParams["host"]
-
 RSpec.describe "Payment gateway tests" do
 
 	context "Functional tests" do
-		testData["functional tests"].each { |test|
-			testCase = "Case " + test["case"].to_s + ". " + test["card type"] + " " + test["currency"]
-			requestData = make_data(test, testData[test["card type"]])
-			puts requestData.to_json
-			it testCase + ": Response code" do
+
+		$testData["functional tests"].each { |test|
+
+			requestData = preparePayment(test)
+			response = apiPost($uri, requestData)
+
+			it "#{test['desc']}: Response code" do
+				expect(response.code).to eq test["expect"]
 			end
-			it testCase + ": Database check" do
+
+			it "#{test['desc']}: Message" do
+				expect(response.body).to match(test["expectmsg"])
 			end
+
+			request = $dbConnect['orders'].find("order"=>requestData)
+			request.each {|d| puts d }
+
+			it "#{test['desc']}: Database check" do
+				expect(request.count).to eq 1
+			end
+
+			it "#{test['desc']}: Correct gateway check" do
+				expect(request.to_a[0]["provider"]).to eq test["expectgw"]
+			end
+
 		}
 	end
 
 	context "Negative tests" do
-		testData["negative tests"].each { |test|
-			testCase = "Case " + test["case"].to_s + ". " + test["card type"] + " " + test["amount"].to_s + test["currency"]
-			requestData = make_data(test, testData[test["card type"]])
-			puts testCase
-			it testCase + ": Response code" do
+
+		$testData["negative tests"].each { |test|
+
+			requestData = preparePayment(test)
+			response = apiPost($uri, requestData)
+
+			it "#{test['desc']}: Response code" do
+				expect(response.code).to eq test["expect"]
 			end
+
+			it "#{test['desc']}: Message" do
+				expect(response.body).to match(test["expectmsg"])
+			end
+
 		}
 	end
 end
